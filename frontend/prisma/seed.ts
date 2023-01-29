7578ad2443b52448d868f1d2cd22d892e669be7c
nuxcodes <44252050+nuxcodes@users.noreply.github.com>
2023-01-29 12:18:21 -0800

    rm Express; use API Route; prisma setup

---
+++ frontend/prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    name: 'Nux',
+    email: 'n@nuxli.dev',
+    maps: {
+      create: [
+        {
+          name: 'First Demo Map',
+          description:
+            'The first game map created by the first 3D scan object file.',
+          scan: {
+            create: {
+              name: 'First 3D Scan',
+              description:
+                "The first stock 3D Scan used to create any map available on MeshCat (I'm rambling).",
+            },
+          },
+        },
+      ],
+    },
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
